58478b8d840cc74d0337a1d483fad75bf75978c1
Miroslav Petrik <miroslav.petrik@thinkcreatix.com>
2018-11-11 21:08:29 +0100

    Updates

---
+++ precache-manifest.578f041908cee8d757fa23d63a6fc027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daf0ecffbe4f13ee64b2",
+    "url": "/react-heapify/static/js/runtime~main.daf0ecff.js"
+  },
+  {
+    "revision": "4c78076cad8034a6b59e",
+    "url": "/react-heapify/static/js/main.4c78076c.chunk.js"
+  },
+  {
+    "revision": "15d139978ce43c81f3b5",
+    "url": "/react-heapify/static/js/1.15d13997.chunk.js"
+  },
+  {
+    "revision": "4c78076cad8034a6b59e",
+    "url": "/react-heapify/static/css/main.e2b84145.chunk.css"
+  },
+  {
+    "revision": "41b22d0d981cfbc0690fb68560ead46c",
+    "url": "/react-heapify/index.html"
+  }
+];
